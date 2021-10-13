b0b66f758fd57eb218fee617248bb1238e96c0a6
TusharKaundal <kaundaltushar@gmail.com>
2021-10-14 00:02:42 +0530

    some changes

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let response = fetch("https://jsonplaceholder.typicode.com/users");
-console.log(response);
-
-setTimeout(() => {
-  console.log(response);
-  const data = response.json();
-  console.log(data);
-});
+let response = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (data) => {
+    console.log(data.json());
+  }
+);
