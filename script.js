16626652efe11cd5e70fd8952adcbb778006625f
TusharKaundal <kaundaltushar@gmail.com>
2021-10-14 09:37:39 +0530

    completed

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-var ul = document.querySelector("ul");
+var table = document.querySelector("table");
 let response = fetch("https://jsonplaceholder.typicode.com/users")
   .then((data) => data.json())
   .then((data) => {
-    data.forEach((element) => {
-      var child = document.createElement("li");
-      var text = document.createTextNode(`${element.name}-${element.email}`);
-      child.appendChild(text);
-      ul.appendChild(child);
+    data.forEach((value) => {
+      var element = document.createElement("tr");
+      var child = `
+      <td>${value.id}</td>
+      <td>${value.name}</td>
+      <td>${value.email}</td>
+      <td>${value.username}</td>
+      `;
+      element.innerHTML = child;
+      table.appendChild(element);
     });
   });
