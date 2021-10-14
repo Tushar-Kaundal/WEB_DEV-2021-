407dec9067a99a241394a08e0d1eac6c2cd5c94b
TusharKaundal <kaundaltushar@gmail.com>
2021-10-14 09:07:07 +0530

    task completed

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+var ul = document.querySelector("ul");
 let response = fetch("https://jsonplaceholder.typicode.com/users")
   .then((data) => data.json())
-  .then((res) => res);
-console.log(response);
+  .then((data) => {
+    data.forEach((element) => {
+      var child = document.createElement("li");
+      var text = document.createTextNode(element.name);
+      child.appendChild(text);
+      ul.appendChild(child);
+    });
+  });
