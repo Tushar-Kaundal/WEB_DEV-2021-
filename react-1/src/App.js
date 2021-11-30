702f11d444ce78ba67bdc7fedfda4011d1907140
TusharKaundal <kaundaltushar@gmail.com>
2021-11-30 11:45:17 +0530

    truncated

---
+++ react-1/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      loreamhgytycucuccyucuutdfytfhfcytctytcyt uy y tfytctyf ytfytcyfcyu
+      ytftycrt
+    </div>
+  );
+}
+
+export default App;
